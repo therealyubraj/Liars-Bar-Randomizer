3f51c74f5909c5060eeb8ac86e49fde95ef97c87
Yubraj Sharma <therealyubraj@gmail.com>
2024-11-02 20:58:14 +0545

    Simple "animations".

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ let bulletNumber = randomInRange(1, 6);
 let currentBullet = 0;
 
 window.onload = () => {
-  console.log("div", document.getElementById("mainDiv"));
   document.body.style.width = window.innerWidth + "px";
   document.body.style.height = window.innerHeight + "px";
   document.getElementById("mainDiv").style.height =
     0.9 * window.innerHeight + "px";
+};
+
+document.getElementById("startBtn").onclick = async () => {
+  reloadingAudio.play();
+  document.getElementById("startBtn").style.display = "none";
+  setTimeout(() => {
+    for (const btn of document.getElementsByClassName("nonStart")) {
+      btn.style.display = "block";
+    }
+  }, 2000); // waiting for the audio to finish
 };
 
 const shotAudio = new Audio("shot.mp3");
@@ -17,15 +26,23 @@ const reloadingAudio = new Audio("reloading.mp3");
 const emptyShotAudio = new Audio("empty_shot.mp3");
 
 const reset = () => {
-  console.log("reset");
   reloadingAudio.play();
   bulletNumber = randomInRange(1, 6);
   currentBullet = 0;
   document.getElementById("mainDiv").style.backgroundColor = "white";
+  document.getElementById("reloadingText").style.display = "block";
+  for (const btn of document.getElementsByClassName("nonStart")) {
+    btn.style.display = "none";
+  }
+  setTimeout(() => {
+    for (const btn of document.getElementsByClassName("nonStart")) {
+      btn.style.display = "block";
+    }
+    document.getElementById("reloadingText").style.display = "none";
+  }, 2000); // waiting for the audio to finish
 };
 
 const increment = () => {
-  console.log("increment", currentBullet, bulletNumber);
   if (bulletNumber === currentBullet) {
     return;
   }
@@ -34,10 +51,21 @@ const increment = () => {
 
   if (bulletNumber === currentBullet) {
     // play sound
-    console.log("DEAD");
     document.getElementById("mainDiv").style.backgroundColor = "red";
     shotAudio.play();
   } else {
+    document.getElementById("mainDiv").style.backgroundColor = "green";
+    setTimeout(() => {
+      document.getElementById("mainDiv").style.backgroundColor = "white";
+    }, 500);
+    for (const btn of document.getElementsByClassName("nonStart")) {
+      btn.style.display = "none";
+    }
+    setTimeout(() => {
+      for (const btn of document.getElementsByClassName("nonStart")) {
+        btn.style.display = "block";
+      }
+    }, 500); // waiting for the audio to finish
     emptyShotAudio.play();
   }
 };
