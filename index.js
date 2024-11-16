2ceec362b2f94a2d17d0a3abf05f0e862c890b4a
llsanketll <slamsal2000@gmail.com>
2024-11-16 15:04:45 +0545

    aaba chai thik cha

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const shuffleAudio = new Audio("assets/audio/shuffle.mp3");
 window.onload = () => {
   document.body.style.width = window.innerWidth + "px";
   document.body.style.height = window.innerHeight + "px";
-  document.getElementById("mainDiv").style.height =
-    0.9 * window.innerHeight + "px";
 };
 
 // document.getElementById("startBtn").onclick = async () => {
@@ -31,20 +29,28 @@ window.onload = () => {
 const shootButton = document.getElementById("shootButton")
 
 const questionMarkList = []
-
+const reactionEmoji = [
+  "🥱",
+  "😳",
+  "😨",
+  "😰",
+  "🫥",
+  "🫡",
+  "😵",
+]
 const positions = [
-  { top: "38%", left: "35.5%" },
-  { top: "61.5%", left: "35.5%" },
-  { top: "73.5%", left: "50%" },
-  { top: "61.5%", left: "65%" },
-  { top: "38%", left: "65%" },
-  { top: "26.5%", left: "50%" },
+  { top: "34%", left: "22%" },
+  { top: "66%", left: "22%" },
+  { top: "82.5%", left: "50%" },
+  { top: "66%", left: "78.5%" },
+  { top: "34%", left: "78.5%" },
+  { top: "17.5%", left: "50%" },
 ]
 for (let i = 0; i < 6; i++) {
   const questionMark = document.createElement("img")
   questionMark.src = "assets/gui/question.png"
   const questionStyle = `
-  width: 150px;
+  width: 9rem;
   position: absolute;
   top: ${positions[i].top};
   left: ${positions[i].left};
@@ -75,30 +81,44 @@ const changeBulletCount = () => {
   document.getElementById("bulletCount").innerHTML = `${currentBullet} / 6`;
 };
 
-const reset = () => {
-  reloadingAudio.play();
-  isShooting = false
-  currentRotation = 360 * 2;
-  shootButton.style.transform = `rotate(${currentRotation}deg)`;
-  bulletNumber = randomInRange(1, 6);
-  currentBullet = 0;
+function updateGameElements() {
   document.getElementById("mainDiv").style.backgroundColor = "white";
-  document.getElementById("reloadingText").style.display = "block";
+  document.getElementById("emojiText").innerHTML = reactionEmoji[0]
   for (const questionMark of questionMarkList) {
-    questionMark.src = "assets/gui/question.png"
-    questionMark.style.display = "block"
+    questionMark.src = "assets/gui/question.png";
+    questionMark.style.display = "block";
   }
   for (const btn of document.getElementsByClassName("nonStart")) {
     btn.style.display = "none";
   }
+  document.getElementById("reloadingText").style.display = "block";
+}
+
+const reset = () => {
+  reloadingAudio.play();
+  isShooting = false;
+  currentBullet = 0;
+  bulletNumber = randomInRange(1, 6);
+  currentRotation = 360 * 2;
+  shootButton.style.transform = `rotate(${currentRotation}deg)`;
+
+  updateGameElements();
+
+  // After sound
   setTimeout(() => {
-    for (const btn of document.getElementsByClassName("nonStart")) {
-      btn.style.display = "block";
-    }
     document.getElementById("reloadingText").style.display = "none";
-    isShooting = false;
-  }, 2000); // waiting for the audio to finish
-  changeBulletCount();
+    changeBulletCount();
+
+    // Setting to 0 without transition
+    currentRotation = 0
+    shootButton.classList.add("disable-transition");
+    shootButton.style.transform = `rotate(${currentRotation}deg)`;
+    // 
+  }, 2000);
+
+  setTimeout(() => {
+    shootButton.classList.remove("disable-transition");
+  }, 2020);
 };
 
 const increment = () => {
@@ -107,6 +127,7 @@ const increment = () => {
   }
   // reloadingAudio.play();
   dhukdhukAudio.play();
+  document.getElementById("emojiText").innerHTML = "🫣"
   currentRotation += 60;
   shootButton.style.transform = `rotate(${currentRotation}deg)`;
   isShooting = true
@@ -117,16 +138,17 @@ const increment = () => {
 };
 const _increment = () => {
   currentBullet++;
-
   if (bulletNumber === currentBullet) {
     // play sound
     document.getElementById("mainDiv").style.backgroundColor = "red";
     questionMarkList[currentBullet - 1].src = "assets/gui/bullet.png"
+    document.getElementById("emojiText").innerHTML = "😵"
     shotAudio.play();
   } else {
     document.getElementById("mainDiv").style.backgroundColor = "green";
     setTimeout(() => {
       document.getElementById("mainDiv").style.backgroundColor = "white";
+      document.getElementById("emojiText").innerHTML = reactionEmoji[currentBullet]
       isShooting = false
       questionMarkList[currentBullet - 1].style.display = "none"
     }, 500);
